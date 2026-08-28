c22dfe0962bfc5b18721548f911d3b22c6c2d059
mjbieren <107464589+mjbieren@users.noreply.github.com>
2026-08-28 21:58:03 +0200

    Refactor MIAF_PREQUAL_COGS_SET.sh with detailed comments
    Updated script to enhance clarity and functionality with detailed comments and settings for the MIAF Prequal workflow.

--- Scripts/18_PREQUAL/18_MIAF_PREQUAL_COGS_SET.sh
+++ Scripts/18_PREQUAL/18_MIAF_PREQUAL_COGS_SET.sh
@@ -1,52 +1,181 @@
 #!/bin/bash
 
-#To create a working prequal conda environment do the following:
-#conda create -n prequal -c bioconda prequal clipkit -c conda-forge tmux 
+# ============================================================
+# MIAF - PREQUAL / Q-INS-i / CLIPKIT
 #
-
-source activate prequal
-
-#Program format can be any order after program path
-
-#MIAF_PATH.out -i <FastaFileFolder> -r <OutputFolder -m <Path to MAFFT> -q <IQTree Path>. -c <Job limit> -q <Thread per Job> -b <Script Path aka this file> -t <Time limit in seconds> -s <System Type>
-
-#-i <Fasta File Directory>\t\t Set the Path to the directory containing your Fasta files: REQUIRED
-#-r <OutputFolderPath>\t\t Set the Output Folder Path where your scripts are moved into: REQUIRED
-#-m <Mafft Path>\t\t Path to Mafft. If installed you can also just use 'mafft': REQUIRED
-#-q <IQTree Path> \t\t Path to IQTree. If Installed you can also use for e.g. iqtree: REQUIRED REQUIRED
-#-c <CPU Limit> \t\t How much threads/jobs can be run simulteanously: NOT REQUIRED
-#-x <IQThread Limit> \t\t How much threads you want IQTree to run on. Keep in mind that the IQThread limit value x the CPU value can not exceed the total amount of threads, unless you want to slow down the machine tremendously 
-#-b <Path to Script>\t\t Path to the original script its' running it. Only needed if you have a time limit: NOT REQUIRED
-#-t <Time limit in seconds> \t\t Time limit of the current job.: NOT REQUIRED %s
-#-s <System Type> \t\t  Either -s s (for Slurm) or -s n (for running it locally). Default is normally: NOT REQUIRED
-#-p  By setting this feature, prequal/qinsi/clipkit will be run (prequal and clipkit has to be installed on the system! Leaving this one out will run mafft/iqtree: NOT REQUIRED
-
-#Program Path
-MIAF_PATH=~/Programs/MIAF/MIAF_Debian_Static.out #Don't Touch
-#OSG output folder aka the fasta files
-INPUT= #Change this (Output of script 17)
+# Combined COGS dataset
+#
+# MIAF:
+# https://github.com/mjbieren/MIAF
+#
+# Using the -p option runs:
+#
+#   Prequal
+#      ↓
+#   MAFFT Q-INS-i
+#      ↓
+#   ClipKIT
+#
+# instead of the standard MAFFT + IQ-TREE workflow.
+# ============================================================
 
 
-#Output Folder
-OUTPUT= #Change this
+# ------------------------------------------------------------
+# SETTINGS
+# ------------------------------------------------------------
 
-#MAFFT
-MAFFT=~/Programs/mafft-linux64/mafft.bat
+# Path to MIAF executable
+MIAF_PATH=##                    # Change this
 
-IQTree=iqtree
+# Input folder containing the filtered COGS FASTA files
+#
+# This is the output from the FilterPPPResult COGS step.
+INPUT=##                        # Change this
 
-SYTEMTYPE=n #Don't touch on gandalf!!!
+# Output folder for the Prequal/Q-INS-i/ClipKIT results
+OUTPUT=##                       # Change this
 
-#JOB Limit
-JOB_LIMIT=50 #will run 50 jobs at the same time
+# Path to MAFFT
+#
+# If MAFFT is available through PATH, simply use:
+#
+#   MAFFT="mafft"
+#
+MAFFT=##                        # Change this
 
-#threads used in MAFFT and other tools
-THREAD_PER_JOB=2 #meaning 50x2 threads are running (aka 100, we have 256 on Gandalf be AWARE!!!)
+# System type
+#
+# n = normal/local execution
+# s = SLURM
+#
+# For normal execution:
+SYSTEMTYPE="n"
 
-#Without IQtree
-${MIAF_PATH} -i ${INPUT} -r ${OUTPUT} -m ${MAFFT} -c ${JOB_LIMIT} -x ${THREAD_PER_JOB} -s ${SYTEMTYPE} -p
+# Maximum number of jobs that MIAF may run simultaneously
+JOB_LIMIT=80
 
-#With IQtree
-#${MIAF_PATH} -i ${INPUT} -r ${OUTPUT} -m ${MAFFT} -c ${JOB_LIMIT} -x ${THREAD_PER_JOB} -s ${SYTEMTYPE} -q ${IQTree} -pi
+# Number of threads used per individual job
+THREAD_PER_JOB=2
 
-conda deactivate
+
+# ------------------------------------------------------------
+# IMPORTANT: CPU USAGE
+# ------------------------------------------------------------
+
+# MIAF can run multiple jobs simultaneously.
+#
+# Approximate maximum CPU usage is:
+#
+#   JOB_LIMIT × THREAD_PER_JOB
+#
+# Here:
+#
+#   80 × 2 = 160 threads
+#
+# Reduce JOB_LIMIT and/or THREAD_PER_JOB if the machine has
+# fewer available CPU threads.
+
+
+# ------------------------------------------------------------
+# PREQUAL MODE
+# ------------------------------------------------------------
+
+# The -p option activates the Prequal workflow.
+#
+# Prequal and ClipKIT must therefore be installed and
+# accessible on the system.
+#
+# MIAF will perform:
+#
+#   Prequal
+#   Q-INS-i alignment using MAFFT
+#   ClipKIT trimming
+#
+# No IQ-TREE path is required for this workflow.
+
+
+# ------------------------------------------------------------
+# MIAF OPTIONS
+# ------------------------------------------------------------
+
+# -i  Input FASTA folder
+#
+# -r  Output folder
+#
+# -m  MAFFT executable/path
+#
+# -c  Maximum number of simultaneous jobs
+#
+# -x  Number of threads per job
+#
+# -s  System type
+#       n = normal/local
+#       s = SLURM
+#
+# -p  Run Prequal + Q-INS-i + ClipKIT workflow
+
+
+# ------------------------------------------------------------
+# CHECK INPUT
+# ------------------------------------------------------------
+
+if [ ! -x "${MIAF_PATH}" ]; then
+    echo "ERROR: MIAF executable not found or not executable:"
+    echo "${MIAF_PATH}"
+    exit 1
+fi
+
+if [ ! -d "${INPUT}" ]; then
+    echo "ERROR: COGS input folder not found:"
+    echo "${INPUT}"
+    exit 1
+fi
+
+
+# ------------------------------------------------------------
+# CREATE OUTPUT DIRECTORY
+# ------------------------------------------------------------
+
+mkdir -p "${OUTPUT}" || {
+    echo "ERROR: Could not create output folder:"
+    echo "${OUTPUT}"
+    exit 1
+}
+
+
+# ============================================================
+# RUN MIAF - PREQUAL WORKFLOW
+# ============================================================
+
+echo
+echo "============================================================"
+echo "Running MIAF - Prequal / Q-INS-i / ClipKIT"
+echo "============================================================"
+echo
+echo "Input:           ${INPUT}"
+echo "Output:          ${OUTPUT}"
+echo "MAFFT:           ${MAFFT}"
+echo "System type:     ${SYSTEMTYPE}"
+echo "Concurrent jobs: ${JOB_LIMIT}"
+echo "Threads/job:     ${THREAD_PER_JOB}"
+echo
+
+
+"${MIAF_PATH}" \
+    -i "${INPUT}" \
+    -r "${OUTPUT}" \
+    -m "${MAFFT}" \
+    -c "${JOB_LIMIT}" \
+    -x "${THREAD_PER_JOB}" \
+    -s "${SYSTEMTYPE}" \
+    -p \
+    || {
+        echo "ERROR: MIAF Prequal workflow failed."
+        exit 1
+    }
+
+
+echo
+echo "============================================================"
+echo "MIAF Prequal / Q-INS-i / ClipKIT completed successfully"
+echo "============================================================"
